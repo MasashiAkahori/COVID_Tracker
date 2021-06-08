d05fa791acbbf6e77da0c6ac60a48ffad59afb2a
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2021-06-08 13:06:15 +0900

    Initial Commit

---
+++ COVID Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID Tracker
+//
+//  Created by 赤堀雅司 on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
